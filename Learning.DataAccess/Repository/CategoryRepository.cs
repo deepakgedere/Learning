6bece1afea2244caf2f48f629f6d360d51202777
Gedere Deepak <deepakgedere@live.com>
2024-03-21 10:28:57 +0530

    Add project files.

---
+++ Learning.DataAccess/Repository/CategoryRepository.cs
@@ -0,0 +1,27 @@
+﻿using Learning.DataAccess.Data;
+using Learning.DataAccess.Repository.IRepository;
+using Learning.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Learning.DataAccess.Repository
+{
+    public class CategoryRepository : Repository<Category>, ICategoryRepository 
+    {
+        private ApplicationDbContext _db;
+        public CategoryRepository(ApplicationDbContext db) : base(db) 
+        {
+            _db = db;
+        }
+       
+        public void Update(Category obj)
+        {
+            _db.Categories.Update(obj);
+        }
+    }
+}
---
+++ Learning.DataAccess/Repository/ProductRepository.cs
@@ -0,0 +1,44 @@
+﻿using Learning.DataAccess.Data;
+using Learning.DataAccess.Repository.IRepository;
+using Learning.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Learning.DataAccess.Repository
+{
+    public class ProductRepository : Repository<Product>, IProductRepository 
+    {
+        private ApplicationDbContext _db;
+        public ProductRepository(ApplicationDbContext db) : base(db) 
+        {
+            _db = db;
+        }
+       
+        public void Update(Product obj)
+        {
+            var objFromDb = _db.Products.FirstOrDefault(u=>u.Id == obj.Id);
+            if(objFromDb != null)
+            {
+                objFromDb.Title = obj.Title;
+                objFromDb.ISBN = obj.ISBN;
+                objFromDb.Price = obj.Price;
+                objFromDb.ListPrice = obj.ListPrice;
+                objFromDb.Price50 = obj.Price50;
+                objFromDb.Price100 = obj.Price100;
+                objFromDb.Description = obj.Description;
+                objFromDb.CategoryId = obj.CategoryId;
+                objFromDb.Author = obj.Author;
+                if(objFromDb.ImageUrl != null)
+                {
+                    objFromDb.ImageUrl = obj.ImageUrl;
+                }
+            }
+        }
+
+    }
+}
