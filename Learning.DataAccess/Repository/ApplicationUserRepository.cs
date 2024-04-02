22c519ed3b3a080e3a25f8b4a36c46bf9da6fd19
Gedere Deepak <deepakgedere@live.com>
2024-04-02 09:33:58 +0530

    added Shoppingcart and ApplicationUser repository

---
+++ Learning.DataAccess/Repository/ApplicationUserRepository.cs
@@ -0,0 +1,27 @@
+﻿using Learning.DataAccess.Data;
+using Learning.DataAccess.Repository.IRepository;
+using Learning.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Learning.DataAccess.Repository
+{
+    public class ApplicationUserRepository : Repository<ApplicationUser>, IApplicationUserRepository
+    {
+        private ApplicationDbContext _db;
+        public ApplicationUserRepository(ApplicationDbContext db) : base(db) 
+        {
+            _db = db;
+        }
+       
+        public void Update(ApplicationUser obj)
+        {
+            _db.ApplicationUsers.Update(obj);
+        }
+    }
+}
