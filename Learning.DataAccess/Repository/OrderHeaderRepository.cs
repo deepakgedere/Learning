35b339bbe87dafc2fa6b476dd09212a611d0ca8a
Gedere Deepak <deepakgedere@live.com>
2024-04-12 14:39:45 +0530

    Cart Summary

---
+++ Learning.DataAccess/Repository/OrderHeaderRepository.cs
@@ -0,0 +1,27 @@
+﻿using Learning.DataAccess.Data;
+using Learning.DataAccess.Repository.IRepository;
+using Learning.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Learning.DataAccess.Repository
+{
+    public class OrderHeaderRepository : Repository<OrderHeader>, IOrderHeaderRepository 
+    {
+        private ApplicationDbContext _db;
+        public OrderHeaderRepository(ApplicationDbContext db) : base(db) 
+        {
+            _db = db;
+        }
+       
+        public void Update(OrderHeader obj)
+        {
+            _db.OrderHeaders.Update(obj);
+        }
+    }
+}
--- Learning.Models/ShoppingCart.cs
+++ Learning.Models/ShoppingCart.cs
@@ -24,5 +24,8 @@ namespace Learning.Models
         [ValidateNever]
         public ApplicationUser ApplicationUser { get; set; }
 
+        [NotMapped]
+        public double Price {  get; set; }
+
     }
 }
